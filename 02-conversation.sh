d9abe4c46cc5e99472d1681483df6270e3e0f71c
kottemadhan <kottemadhan20@email.com>
2025-10-04 23:37:07 +0530

    some message

---
+++ 02-conversation.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+
+
+echo "madhan" Hi anil, how are you?"
+echo "anil" Hello madhan i am fine. how are you"
+echo "madhan"How are you today?"
+echo "anil"You said you're feeling $feeling. Hope you have a great day!"
