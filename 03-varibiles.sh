35989b20d65778cd0acdb72b74028539a0a8cfbe
madhan kotte <madhankotte143@email.com>
2025-10-05 21:00:39 +0530

    variables

---
+++ 03-varibiles.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+persion1;madhan
+persion2:anil
+
+echo "persion1" Hi anil, how are you?"
+echo "persion2" Hello madhan i am fine. how are you"
+echo "persion1"How are you today?"
+echo "persion2"You said you're feeling $feeling. Hope you have a great day!"
---
+++ 04-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+persion1;madhan
+persion2:anil
+
+echo "persion1" welcome to india persion2"
+echo "persion2" Hello persion1 i am fine. how are you"
+echo "persion1"How are you today?"
+echo "persion2"You said you're feeling $feeling. Hope you have a great day!"
