d64803d387bf454de4befd77f914aeb421079d18
madhankotte <madhankotte20@email.com>
2025-10-07 22:56:21 +0530

    variables

--- 09-data-types.sh
+++ 09-data-types.sh
@@ -8,6 +8,9 @@ sum=$(($Number1+$Number2+$NAME))
 
 echo "Sum is : $sum"
 
+# size = 4, Max index = 3
 LEADERS=("Steve" "Bill" "Elon" "Jeff")
 
 echo "All leaders: ${LEADERS[@]}"
+echo "First leader: ${LEADERS[0]}"
+echo "First leader: ${LEADERS[10]}"
