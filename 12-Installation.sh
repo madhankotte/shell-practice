b4886cafdd9453cd59cedd33e37e813d3d1b6bbb
madhankotte <madhankotte20@email.com>
2025-10-09 11:07:20 +0530

    12-installation.sh

--- 12-Installation.sh
+++ 12-Installation.sh
@@ -2,13 +2,15 @@
 
   USERID=$(id -u)
     if [ "$USERID" -ne 0 ]; then
-    echo "Error::Please run this script with root or privilege."
+       echo "Error::Please run this script with root or privilege."
+       exit 1 # failure is other than 0
     fi
 
-    dnf install mysql -y
+        dnf install mysql -y
     
     if [ $? -ne 0 ]; then
         echo " Error::Failed to install MySQL."
+        exit 1
         else
         echo "MySQL installed successfully."
-        fi
+    fi
