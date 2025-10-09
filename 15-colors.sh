5575fa131c371f35ba9ee9c3bf51ff6ec56c87a8
madhankotte <madhankotte20@email.com>
2025-10-09 17:55:21 +0530

    15-colors.sh

--- 15-colors.sh
+++ 15-colors.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
  USERID=$(id -u)
+ R='\e[31m' # Red
+G='\e[32m' # Green
+B='\e[34m' # Blue
+Y='\e[33m' # Yellow
+N='\e[0m'  # No Color
+
     if [ "$USERID" -ne 0 ]; then
        echo "Error::Please run this script with root or privelege."
        exit 1 # failure is other than 0
